8da7cad30f78d482743d72c129de03266529bce7
BhelPuriPanda <boreover582@gmail.com>
2025-06-01 14:22:26 +0530

    Deployment takes foreverr

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/TYPEGAME/',
+  base: 'typegame-frontend',
   plugins: [react(),tailwindcss()],
 })
